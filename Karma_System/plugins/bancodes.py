8d57da2c509ff68f1268bb871929611bd83b5991
maxboffical <135422395+maxboffical@users.noreply.github.com>
2023-06-03 15:04:20 +0530

    Add files via upload

---
+++ Karma_System/plugins/bancodes.py
@@ -0,0 +1,26 @@
+   
+help_plus = """ Here is Help for **Bancodes** :-
+**❂** `{KXKx00}` **SCAMMER.**
+**❂** `{KXKx01}` **SPAM ADDING MEMBER.**
+**❂** `{KXKx03}` **ABUSE SPAM.**
+**❂** `{KXKx04}` **NSFW SPAMMER.**
+**❂** `{KXKx06}` **IMPERSONATION.**
+**❂** `{KXKx07}` **MD/BTC SCAM.**
+**❂** `{KXKx08}` **ADDING SPAMBOTS.**
+**❂** `{KXKx10}` **ILLEGAL.**
+**❂** `{KXKx11}` **PHISHING.**
+**❂** `{KXKx12}` **FRAUD PROMOTION  [ANY KIND].**
+**❂** `{KXKx13}` **CYBER THREATENING / CYBER BULLY.**
+**❂** `{KXKx14}` **CHILD ABUSE.**
+**❂** `{KXKx15}` **BAN EVASION.**
+**❂** `{KXKx16}` **SPAMBOT.**
+**❂** `{KXKx17}` **RAID INITIALIZOR.** 
+**❂** `{KXKx18}` **RAID PARTICIPANT.**
+**❂** `{KXKx19}` **KRIMINALANT.**
+**❂** `{KXKx20}` **SPAMMING VIOLENT CONTENT.**
+**❂** `{KXKx21}` **HATE SPEECH AGAINST NATIONALITY.**
+**❂** `{KXKx22}` **RAID/SPAM INFLAMMER.**
+**❂** `{KXKx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
+**❂** `{KXKx24}` **PAID GIRL.**
+"""
+__plugin_name__ = "bancodes"
